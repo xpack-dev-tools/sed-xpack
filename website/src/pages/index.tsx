c297c6d2518e6ab5728d0be87c850bc6f55e7d73
Liviu Ionescu <ilg@livius.net>
2024-10-15 03:34:22 +0300

    website: updates

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout 
-      title={`Welcome to sed`}
+      title="Welcome!"
       description="A binary package with the sed executables">
       <HomepageHeader />
       <main>
